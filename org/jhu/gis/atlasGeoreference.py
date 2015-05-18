5e6defb7d8033a5e00351850afc3a7128e82e1d1
bbrosius <benbrosius@gmail.com>
2015-05-18 15:40:34 -0400

    Initial implementation of the toolbar to georeference and clip the scanned atlas images.

--- org/jhu/gis/atlasGeoreference.py
+++ org/jhu/gis/atlasGeoreference.py
@@ -27,10 +27,13 @@ source_pnt = "'2.372 29.044';'28.782 29.032';'28.797 2.59';'2.382 2.606'"
 
 ##Define target control points
 target_pnt = "'" + str(extent.XMin) + " " + str(extent.YMax) + "';'" + str(extent.XMax) + " " + str(extent.YMax) + "';'" + str(extent.XMax) + " " + str(extent.YMin) + "';'" + str(extent.XMin) + " " + str(extent.YMin) + "'"
+extentStr = str(extent.XMin) + " " + str(extent.YMin) + " " + str(extent.XMax) + " " + str(extent.YMax)
 
 print "Georeferencing: " + imageFile
 print "Target points: " + target_pnt
-arcpy.Warp_management(raster, source_pnt, target_pnt, referencedFile, "POLYORDER1",\
+
+arcpy.Warp_management("1N-1E.tif", "'2.38208950617 29.0419241255';'28.7841728395 29.0210907922';'28.8050061728 2.58671579218';'2.38521450617 2.59713245885'", "'1421102.0 599458.0';'1426382.0 599458.0';'1426382.0 594178.0';'1421102.0 594178.0'\
+", referencedFile, "POLYORDER1",\
                           "NEAREST")
 
 clippedFile = baseImageDir + "/" + imageFile + "clipped.tif"
---
+++ org/jhu/gis/geoclip/Install/geoclip_addin.py
@@ -0,0 +1,139 @@
+import arcpy
+import pythonaddins
+import ntpath
+import os
+
+imageFile = ""
+pointCount = 0
+dialogShown = False
+mapExtent = ""
+shapeFile = ""
+saveDir = ""
+dataDir = "Rectified Data"
+
+class RunButtonClass(object):
+    """Implementation for geoclip_addin.runButton (Button)"""
+    def __init__(self):
+        self.enabled = True
+        self.checked = False
+    def onClick(self):
+        global pointCount
+        if pointCount < 4:
+            pythonaddins.MessageBox("Select four map points before running", "Error", 0);
+        else:
+            global imageFile
+            global shapeFile
+            global  mapExtent
+            global dialogShown
+            arcpy.env.overwriteOutput = True
+
+            arcpy.env.outputCoordinateSystem = arcpy.Describe(shapeFile).spatialReference
+            print imageFile[:-4]
+            where = "IndexSht = '" + imageFile[:-4].lower() + "'"
+            rows = arcpy.SearchCursor(shapeFile, where, "", "Shape; IndexSht", "")
+
+            row = rows.next()
+            extent = row.shape.extent
+            parentDir = os.path.join(saveDir, dataDir)
+            if not os.path.exists(parentDir):
+                os.makedirs(parentDir)
+            referencedFile = os.path.join(parentDir, imageFile[:-4] + "_referenced.tif")
+
+            ##Define target control points
+            target_pnt = "'" + str(extent.XMin) + " " + str(extent.YMax) + "';'" + str(extent.XMax) + " " + str(extent.YMax) + "';'" + str(extent.XMax) + " " + str(extent.YMin) + "';'" + str(extent.XMin) + " " + str(extent.YMin) + "'"
+
+            print "Georeferencing: " + imageFile
+            print "Target points: " + target_pnt
+            print "Map extent: " + mapExtent
+            print "Reference file: " + referencedFile
+            arcpy.Warp_management(imageFile, mapExtent, target_pnt, referencedFile, "POLYORDER1",
+                                      "NEAREST")
+
+            clippedFile = os.path.join(parentDir, imageFile[:-4] + "_clipped.tif")
+            extentStr = str(extent.XMin) + " " + str(extent.YMin) + " " + str(extent.XMax) + " " + str(extent.YMax)
+            arcpy.Clip_management(referencedFile, extentStr, clippedFile, "#", "#", "NONE")
+
+            #Reset variables
+            pointCount = 0
+            mapExtent = ""
+            dialogShown = False
+class SelectImageBox(object):
+    """Implementation for geoclip_addin.selectImageBox (ComboBox)"""
+    def __init__(self):
+        self.editable = True
+        self.enabled = True
+        self.dropdownWidth = 'WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW'
+        self.width = 'WWWWWWWWWWWWWWWWWWWWWWWWWWW'
+        self.mxd = arcpy.mapping.MapDocument('current')
+        layers = arcpy.mapping.ListLayers(self.mxd)
+        self.items = []
+        for layer in layers:
+            self.items.append(layer.name)
+    def onSelChange(self, selection):
+        global imageFile
+        global saveDir
+        imageFile = selection
+        self.mxd = arcpy.mapping.MapDocument('current')
+        layers = arcpy.mapping.ListLayers(self.mxd, selection)
+        for layer in layers:
+            saveDir = ntpath.dirname(layer.dataSource)
+    def onFocus(self, focused):
+        if focused:
+            self.mxd = arcpy.mapping.MapDocument('current')
+            layers = arcpy.mapping.ListLayers(self.mxd)
+            self.items = []
+            for layer in layers:
+                self.items.append(layer.name)
+
+class SelectPointsTool(object):
+    """Implementation for geoclip_addin.selectPointsTool (Tool)"""
+    def __init__(self):
+        self.enabled = True
+        self.shape = "NONE"
+        self.cursor = 3
+    def onMouseDownMap(self, x, y, button, shift):
+        global mapExtent
+        global pointCount
+        pointCount += 1
+        if pointCount < 4:
+            mapExtent += "'" + str(x) + " " + str(y) + "';"
+        else:
+            mapExtent += "'" + str(x) + " " + str(y) + "'"
+        if pointCount == 1:
+            pythonaddins.MessageBox("Select the top right corner of the map", "Selected Map Points", 0)
+        elif pointCount == 2:
+            pythonaddins.MessageBox("Select the bottom right corner of the map", "Selected Map Points", 0)
+        elif pointCount == 3:
+            pythonaddins.MessageBox("Select the bottom left corner of the map", "Selected Map Points", 0)
+    def onMouseMove(self, x, y, button, shift):
+        global dialogShown
+        if not dialogShown:
+            pythonaddins.MessageBox("Select the top left corner of the map", "Selected Map Points", 0)
+            dialogShown = True
+    def onDeactivate(self):
+        global mapExtent
+        global pointCount
+        pointCount = 0
+        mapExtent = ""
+
+class ShapeFileComboBox(object):
+    def __init__(self):
+        self.editable = True
+        self.enabled = True
+        self.dropdownWidth = 'WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW'
+        self.width = 'WWWWWWWWWWWWWWWWWWWWWWWWWWW'
+        self.mxd = arcpy.mapping.MapDocument('current')
+        layers = arcpy.mapping.ListLayers(self.mxd)
+        self.items = []
+        for layer in layers:
+            self.items.append(layer.name)
+    def onSelChange(self, selection):
+        global shapeFile
+        shapeFile = selection
+    def onFocus(self, focused):
+        if focused:
+            self.mxd = arcpy.mapping.MapDocument('current')
+            layers = arcpy.mapping.ListLayers(self.mxd)
+            self.items = []
+            for layer in layers:
+                self.items.append(layer.name)
